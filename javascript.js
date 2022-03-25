5c5eb901c2f6f4326c51366d1a9115692408f8b8
Osserc <rstefanofil@hotmail.com>
2022-03-25 15:27:55 +0100

    Add slider
    Added a slider to set the size of the new whiteboard

--- javascript.js
+++ javascript.js
@@ -1,6 +1,16 @@
 let k = 16;
-
 generateGrid (k);
+
+let slider = document.getElementById("myRange");
+let gridValue = slider.value;
+document.getElementById("gridValue").innerHTML = gridValue + 'x' + gridValue;
+
+slider.oninput = function() {
+  gridValue = this.value;
+  document.getElementById("gridValue").innerHTML = gridValue + 'x' + gridValue;
+}
+
+
 
 function generateGrid (k) {
     g = k;
