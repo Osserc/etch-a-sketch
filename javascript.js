2e90092144b51d4abd73d914b251f1ad595e92d8
Osserc <rstefanofil@hotmail.com>
2022-03-25 17:39:49 +0100

    Add full slider functionality
    The user can now set the number of squares via the slider

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 let k = 16;
 let g = k;
-
+let flexPercentage = ((1 / k) * 100) + '%';
 generateGrid (k);
 
 function generateGrid (k) {
@@ -14,7 +14,7 @@ function generateGrid (k) {
         grid.appendChild(cell);
     }
 
-    let flexPercentage = ((1 / k) * 100) + '%';
+    flexPercentage = ((1 / k) * 100) + '%';
     document.documentElement.style.setProperty('--flex-basis-percentage', flexPercentage);
 }
 
@@ -32,9 +32,13 @@ function refreshGrid (k) {
 }
 
 const slider = document.querySelector('.slider');
-let gridValue = slider.value;
+let gridValue = document.querySelector('.slider').value;
 document.querySelector(".display").innerHTML = gridValue + 'x' + gridValue;
 
 slider.oninput = function() {
     document.querySelector(".display").innerHTML = this.value + 'x' + this.value;
+    gridValue = document.querySelector('.slider').value;
 }
+
+const refresh = document.querySelector('.regenerate');
+refresh.addEventListener('click', function () {refreshGrid (gridValue)});
