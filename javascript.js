f22ed1fd9cdeae4a28a100e01c2a7c07d54ffcbb
Osserc <rstefanofil@hotmail.com>
2022-03-25 19:07:36 +0100

    Add coloring functionality
    The project is essentially done, all that remains is customization,
    polish adding more advanced features

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@ let k = 16;
 let g = k;
 let flexPercentage = ((1 / k) * 100) + '%';
 generateGrid (k);
+chameleonifyCells ();
 
 function generateGrid (k) {
     g = k;
@@ -29,16 +30,32 @@ function clearGrid (k) {
 function refreshGrid (k) {
     clearGrid (g);
     generateGrid (k);
+    chameleonifyCells ();
 }
 
+//set the default text in the reset button
 const slider = document.querySelector('.slider');
 let gridValue = document.querySelector('.slider').value;
 document.querySelector(".display").innerHTML = gridValue + 'x' + gridValue;
 
+//dynamically change the text in the reset button
 slider.oninput = function() {
     document.querySelector(".display").innerHTML = this.value + 'x' + this.value;
     gridValue = document.querySelector('.slider').value;
 }
 
+//make the button refresh the grid with the desired dimension
 const refresh = document.querySelector('.regenerate');
 refresh.addEventListener('click', function () {refreshGrid (gridValue)});
+
+function randomizeColor () {
+    const randomColor = "#"+((1<<24)*Math.random()|0).toString(16);
+    //document.body.style.backgroundColor = randomColor;
+    event.target.style.backgroundColor = randomColor;
+}
+
+function chameleonifyCells () {
+    const changeColor = document.querySelectorAll('.cell').forEach(changeColor => changeColor.addEventListener('mouseenter', function () {
+        randomizeColor ();
+    }));
+}
